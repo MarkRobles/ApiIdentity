cb25b5d19261a459af313cb6471550070835b677
Marcos Robles <ecko_lob@outlook.com>
2020-07-27 22:45:06 -0500

    Agregar archivos de proyecto.

---
+++ WebApi/Models/AppUsersDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class AppUsersDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppUsersDbContext()
+        : base("SystemUsers", throwIfV1Schema: false)
+        {
+        }
+
+        public static AppUsersDbContext Create()
+        {
+            return new AppUsersDbContext();
+        }
+    }
+}
